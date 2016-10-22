f9cb033a3b36280d743fba743c9316c269410be1
Sandip Shrestha <sandipshrestha@sandips-macbook-pro.local>
2016-10-22 12:55:10 -0700

    Initial Commit

---
+++ HelloWorldLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldLabel
+//
+//  Created by Sandip Shrestha on 10/22/16.
+//  Copyright © 2016 SKEnterprise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
